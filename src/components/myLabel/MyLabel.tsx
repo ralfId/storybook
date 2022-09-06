d2b0b18c37fd595ed2fada0688703a339ecd787d
Rafael-ID <49356385+ralfId@users.noreply.github.com>
2022-09-06 17:01:58 -0600

    Learn about storybook DONE

---
+++ src/components/myLabel/MyLabel.tsx
@@ -0,0 +1,33 @@
+import { AllCaps } from '../../stories/components/MyLabel.stories';
+import './myLabel.css';
+
+export interface MyLabelProps {
+    /**
+   * This is the label text
+   */
+    label: string;
+    /**
+   * this is the font size of the label
+   */
+    size?: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * this is to capitaliza the label text
+     */
+    allCaps?: boolean;
+    /**
+     * Basic color for label text
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * This is a custom label text color
+     */
+    customColor?: string;
+}
+
+const colorType = {}
+
+export const MyLabel = ({ label = 'My Label text', size = 'normal', color = 'primary', allCaps = false, customColor}: MyLabelProps) => {
+    return (
+        <span className={`${size} text-${color} ${ (allCaps) && 'uppercase' }` } style={{ color: customColor  }}> {label} </span>
+    )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,40 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { MyLabel } from "../../components/myLabel/MyLabel";
+
+export default{
+    title: 'ReactCourse/MyLabel',
+    component: MyLabel,
+    argTypes: {
+        size: { control: { type: 'select' } },
+        allCaps: { control: { type: 'boolean'  }, defaultValue: false },
+        color: { control: { type: 'select' } },
+    }
+} as ComponentMeta<typeof MyLabel>;
+
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel { ...args }/>
+
+export const Basic = Template.bind({});
+//Default values
+Basic.args={
+    label: 'custom label text',
+    size: 'normal'
+}
+export const AllCaps = Template.bind({});
+AllCaps.args={
+    label: 'custom label text',
+    allCaps: true,
+    size: 'normal'
+}
+export const Secundary = Template.bind({});
+Secundary.args={
+    label: 'custom label text',
+    color: 'secondary',
+    size: 'normal'
+}
+
+export const Tertiary = Template.bind({});
+Tertiary.args={
+    label: 'custom label text',
+    color: 'tertiary',
+    size: 'normal'
+}
